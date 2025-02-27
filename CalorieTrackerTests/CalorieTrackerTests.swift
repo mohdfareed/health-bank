2d05562c83e40346109b20b7641d967cf4344ff1
Mohammed Fareed <mohdf.fareed@icloud.com>
2025-02-26 20:04:34 -0500

    Initial Commit

---
+++ CalorieTrackerTests/CalorieTrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  CalorieTrackerTests.swift
+//  CalorieTrackerTests
+//
+//  Created by Mohammed Fareed on 2/26/25.
+//
+
+import Testing
+
+struct CalorieTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
